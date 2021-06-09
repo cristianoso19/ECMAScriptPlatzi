0e3f09814c4b829e2c6fba8f0efa5c109196c769
Cristian Sacta Ry <cristianoso19@gmail.com>
2021-06-09 18:41:49 -0500

    LET and CONST

--- ec6/index.js
+++ ec6/index.js
@@ -25,16 +25,71 @@ let world = "World";
 let epicPhrase = hello + " " + world;
 console.log(epicPhrase);
 
+//ES6 TEMPLATE LITERALS
+let epicPhrase2 = `${hello} ${world}`;
+console.log(epicPhrase2);
+
+/*
+ *MULTILINEA EN STRINGS
+ */
+//ES5
 let lorem =
   "Qui consequatur. Commodi. Ipsum vel duis yet minima \n" +
   "otra frase epica que necesitamos.";
 console.log(lorem);
 
-//ES6
-let epicPhrase2 = `${hello} ${world}`;
-console.log(epicPhrase2);
-
+//ES6 no es necesario el caracter "\n" ni "+"
 let lorem2 = `otra frase epica que necesitamos
 ahora es otra frase epica
 `;
 console.log(lorem2);
+
+/*
+ *DESTRUCTURACION DE ELEMENTOS
+ */
+
+let person = {
+  name: "oscar",
+  age: 32,
+  country: "MX",
+};
+//ES5
+console.log(person.name, person.age);
+//ES6
+//Ahora podemos acceder a las propiedades
+//de los elementos con let
+let { name, age, country } = person;
+console.log(name, age, country);
+
+/*
+ *OPERADOR DE PROPAGACION
+ */
+//ES6 "..." Para poder unir los elementos de
+//un array en otro
+let team1 = ["Oscar", "Julian", "Ricardo"];
+let team2 = ["Valeria", "Yesica", "Camila"];
+
+let education = ["David", ...team1, ...team2];
+
+console.log(education);
+
+/*
+ *LET
+ */
+{
+  var globalVar = "Global Var";
+}
+
+{
+  let globalLet = "Global Let";
+  console.log(globalLet);
+}
+
+console.log(globalVar);
+
+/*
+ *CONST
+ */
+const a = "b";
+a = "a";
+console.log(a);
