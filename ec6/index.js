0f3e4a9b94fdf898d06e42b6b3348c44a24cdc52
Cristian Sacta Ry <cristianoso19@gmail.com>
2021-06-09 16:12:35 -0500

    Concatenar

--- ec6/index.js
+++ ec6/index.js
@@ -1,4 +1,6 @@
-//DEFAULT PARAMS
+/*
+ *DEFAULT PARAMS
+ */
 //ES5
 function newFunction(name, age, country) {
   var name = name || "cris";
@@ -14,3 +16,25 @@ function newFunction2(name = "cris", age = 32, country = "EC") {
 newFunction();
 newFunction2();
 newFunction2("jen", 23, "EC");
+/*
+ *CONCATENATE
+ */
+//ES5
+let hello = "Hello";
+let world = "World";
+let epicPhrase = hello + " " + world;
+console.log(epicPhrase);
+
+let lorem =
+  "Qui consequatur. Commodi. Ipsum vel duis yet minima \n" +
+  "otra frase epica que necesitamos.";
+console.log(lorem);
+
+//ES6
+let epicPhrase2 = `${hello} ${world}`;
+console.log(epicPhrase2);
+
+let lorem2 = `otra frase epica que necesitamos
+ahora es otra frase epica
+`;
+console.log(lorem2);
