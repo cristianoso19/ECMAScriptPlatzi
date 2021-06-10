bd90d583764b54d57a37488c64dc8670c744321c
Cristian Sacta Ry <cristianoso19@gmail.com>
2021-06-09 19:12:37 -0500

    Clases

--- ec6/index.js
+++ ec6/index.js
@@ -132,6 +132,38 @@ const listOfNames4 = name => {
 
 const square = num => num * num;
 
+/*
+ *PROMESAS / ASINCRONISMO
+ */
 
+const helloPromise = () => {
+  return new Promise((resolve, reject) => {
+    if (false) {
+      resolve('Hey!');
+    } else {
+      reject('Ups!!');
+    }
+  });
+}
 
+helloPromise()
+  .then(response => console.log(response))
+  .catch(error => console.log(error));
 
+/*
+ *CLASES
+ */
+class calculator {
+  constructor() {
+    this.valueA = 0;
+    this.valueB = 0;
+  }
+  sum(valueA, valueB) {
+    this.valueA = valueA;
+    this.valueB = valueB;
+    return this.valueA + this.valueB;
+  }
+}
+
+const calc = new calculator();
+console.log(calc.sum(2, 2));
