96246b0ea5fe89fc29392322090aaa9a50d1acb5
Cristian Fedora <cristianoso19@gmail.com>
2021-06-11 22:19:36 -0500

    Object Values

--- es8/index.js
+++ es8/index.js
@@ -13,3 +13,16 @@ console.log(entries);
 // Ahora podemos hacer de .lenght para
 // acceder a cuantos elementos tiene el objeto
 console.log(entries.length);
+/*
+ *Object values
+ */
+// Devuelve los valores de un objeto a un arreglo
+const data = {
+  frontend : "Cris",
+  backend : "Jen",
+  design : "Ana",
+};
+
+const values = Object.values(data);
+console.log(values);
+console.log(values.length);
